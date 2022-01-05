8153241e7aa2e63536fd5d4da85468ed961e798f
pauloWeslleY13 <weslleyspfc13@outlook.com>
2022-01-05 17:18:22 -0300

    curso-ts proj-calc aula74

--- assets/scripts/calculator/CalculatorController.js
+++ assets/scripts/calculator/CalculatorController.js
@@ -4,6 +4,48 @@ export default class CalculatorController {
     constructor(screen = new Screen()) {
         this.screen = screen;
         new DateHour();
+        this.eventButtons();
+    }
+    eventButtons() {
+        document.querySelectorAll("#teclado button").forEach(element => {
+            element.addEventListener("click", (event) => {
+                const target = event.target;
+                switch (target.id) {
+                    case "zero":
+                    case "um":
+                    case "dois":
+                    case "tres":
+                    case "quatro":
+                    case "cinco":
+                    case "seis":
+                    case "sete":
+                    case "oito":
+                    case "nove":
+                        this.addNumber(Number(target.dataset.valor));
+                        break;
+                    case "adicao":
+                    case "subtracao":
+                    case "divisao":
+                    case "multiplicacao":
+                        break;
+                    case "ponto":
+                        break;
+                    case "limpar":
+                        break;
+                    case "desfazer":
+                        break;
+                    case "porcentagem":
+                        break;
+                    case "igual":
+                        break;
+                    default:
+                        break;
+                }
+            });
+        });
+    }
+    addNumber(numbers) {
+        this.screen.content = numbers.toString();
     }
 }
 //# sourceMappingURL=CalculatorController.js.map
--- assets/typescript/calculator/CalculatorController.ts
+++ assets/typescript/calculator/CalculatorController.ts
@@ -4,5 +4,56 @@ import Screen from './Screen.js'
 export default class CalculatorController {
    constructor(private screen = new Screen()) {
       new DateHour();
+      this.eventButtons();
+   }
+
+   eventButtons(): void {
+      document.querySelectorAll("#teclado button").forEach(element => {
+         element.addEventListener("click", (event: Event) => {
+            const target = event.target as HTMLButtonElement;
+            switch (target.id) {
+               case "zero":
+               case "um":
+               case "dois":
+               case "tres":
+               case "quatro":
+               case "cinco":
+               case "seis":
+               case "sete":
+               case "oito":
+               case "nove":
+                  this.addNumber(Number(target.dataset.valor));
+                  break;
+               case "adicao":
+               case "subtracao":
+               case "divisao":
+               case "multiplicacao":
+
+                  break;
+               case "ponto":
+
+                  break;
+               case "limpar":
+
+                  break;
+               case "desfazer":
+
+                  break;
+               case "porcentagem":
+
+                  break;
+               case "igual":
+
+                  break;
+
+               default:
+                  break;
+            }
+         });
+      });
+   }
+
+   addNumber(numbers:  number): void {
+      this.screen.content = numbers.toString();
    }
 }
